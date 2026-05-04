fc186c395e7c4477953c5fec8ecb1504d45b3a22
Daisie Huang <daisieh@gmail.com>
2026-05-04 10:53:16 -0700

    Update auth.py

--- src/authx/auth.py
+++ src/authx/auth.py
@@ -340,6 +340,8 @@ def get_vault_token_for_service(service=SERVICE_NAME, vault_url=VAULT_URL, appro
         raise CandigAuthError("no role_id found")
 
     # get the secret_id
+    print(f"hola2 {secret_id}")
+
     if secret_id is None:
         url = f"{vault_url}/v1/auth/approle/role/{service}/secret-id"
         headers = { "X-Vault-Token": approle_token }
@@ -349,7 +351,6 @@ def get_vault_token_for_service(service=SERVICE_NAME, vault_url=VAULT_URL, appro
             secret_id = response.json()["data"]["secret_id"]
         else:
             raise CandigAuthError(f"secret_id: {response.text}")
-        print("hola2")
 
         # swap the role_id and service_id for a token
         data = {
@@ -364,6 +365,7 @@ def get_vault_token_for_service(service=SERVICE_NAME, vault_url=VAULT_URL, appro
             return response.json()["auth"]["client_token"]
         else:
             raise CandigAuthError(f"login: {response.text}")
+    print("hola4")
     return None
 
 
