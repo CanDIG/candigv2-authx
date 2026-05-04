855052a7d78c430dde5df76604dd6f6e210c2012
Daisie Huang <daisieh@gmail.com>
2026-05-04 10:56:22 -0700

    Update auth.py

--- src/authx/auth.py
+++ src/authx/auth.py
@@ -343,6 +343,7 @@ def get_vault_token_for_service(service=SERVICE_NAME, vault_url=VAULT_URL, appro
     print(f"hola2 {secret_id} {service}")
 
     if secret_id is None:
+        print("hola0")
         url = f"{vault_url}/v1/auth/approle/role/{service}/secret-id"
         headers = { "X-Vault-Token": approle_token }
         response = requests.post(url=url, headers=headers)
